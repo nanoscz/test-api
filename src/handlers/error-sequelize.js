1c34d5400955fa8e6d6ad58e6887beeb3d19e107
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-06 15:06:59 -0400

    Improve errorSequelize

--- src/handlers/error-sequelize.js
+++ src/handlers/error-sequelize.js
@@ -1,23 +1,21 @@
 'use strict'
 
 module.exports = function errorSequelize (err, req, res, next) {
-  const payload = {
-    error: {
-      name: err.name,
-      internalMessage: err.parent.sqlMessage
-    }
+  const error = {
+    name: err.name,
+    internalMessage: err.parent.sqlMessage
   }
   switch (err.name) {
     case 'SequelizeUniqueConstraintError':
-      payload.error.message = 'The number of ci has already been registered.'
-      res.json(payload)
+      error.message = 'The number of ci has already been registered.'
+      res.status(400).send(error)
       break
     case 'SequelizeDatabaseError':
-      payload.error.message = 'The type is not valid.'
-      res.json(payload)
+      error.message = err
+      res.status(400).send(error)
       break
     default:
-      next(err)
+      res.json(err)
       break
   }
 }
